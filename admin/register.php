eb25e702afb7a29e3e860fb3174b183ad2e11d33
unknown <justshamsulalom@gmail.com>
2020-11-06 21:41:50 +0600

    Login reset password and create account done

--- admin/register.php
+++ admin/register.php
@@ -1,3 +1,8 @@
+<?php
+  include "inc/db.php";
+  session_start();
+  ob_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -29,38 +34,70 @@
       <p class="login-box-msg">Register a new membership</p>
 
       <form action="" method="POST" enctype="multipart/form-data">
+        
         <div class="input-group mb-3">
-          <input type="text" class="form-control" placeholder="Full name">
+          <input type="text" class="form-control" placeholder="Full name" name="name" required="required">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="email" required="required">
+          <div class="input-group-append">
+            <div class="input-group-text">
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="pass" required="required">
+          <div class="input-group-append">
+            <div class="input-group-text">
             </div>
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="email" class="form-control" placeholder="Email">
+          <input type="password" class="form-control" placeholder="Retype password" name="repass" required="required">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-envelope"></span>
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password">
+          <input type="text" class="form-control" placeholder="Phone Number" name="phone" required="required">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Retype password">
+          <input type="text" class="form-control" placeholder="Address" name="address" required="required">
           <div class="input-group-append">
             <div class="input-group-text">
-              <span class="fas fa-lock"></span>
             </div>
           </div>
         </div>
+
+        <div class="input-group mb-3">
+          <select name="gender" id="" class="form-control">
+            <option value="1">Male</option>
+            <option value="2">Female</option>
+            <option value="3">Other</option>
+          </select>
+          <div class="input-group-append">
+            <div class="input-group-text">
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="file" class="form-control-files" placeholder="Profile Picture" name="image" required="required">
+        </div>
+
         <div class="row">
           <div class="col-8">
             <div class="icheck-primary">
@@ -72,11 +109,65 @@
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block">Register</button>
+            <button type="submit" name="sbm" class="btn btn-primary btn-block">Register</button>
           </div>
           <!-- /.col -->
         </div>
+
       </form>
+
+      <?php
+        if (isset($_POST['sbm'])) {
+          $name     = $_POST['name'];
+          $email    = $_POST['email'];
+          $pass     = $_POST['pass'];
+          $repass   = $_POST['repass'];
+          $phone    = $_POST['phone'];
+          $address  = $_POST['address'];
+          $gender   = $_POST['gender'];
+          //image preparing
+          $imagename    = $_FILES['image']['name'];
+          $imagetype    = $_FILES['image']['type'];
+          $imagesize    = $_FILES['image']['size'];
+          $imagetemp    = $_FILES['image']['tmp_name'];
+          //extension
+          $supportedext = array("jpg","jpeg","png");
+          $nameexplode  = strtolower(end(explode('.', $imagename) ) );
+          //form error
+          $error = array();
+          if ( $name > 4) {
+            $error = "User Name is Too Short";
+          }
+          if ( $pass != $repass) {
+            $error = "Password Dose Not Match";
+          }
+          if (empty($error)) {
+
+            $img_name = rand(1, 99999).'_'.$imagename;
+            move_uploaded_file($imagetemp, "img/user/$img_name");
+
+            $sha1= sha1($pass);
+
+
+
+            $sql = "INSERT INTO user (user_image,user_name,user_email,user_password,user_phone,user_address,user_role,user_status,user_gender,user_join_date) VALUES ('$img_name','$name','$email','$sha1','$phone','$address',0,3,'$gender',now())";
+            $query = mysqli_query($db , $sql);
+            if ($query) {
+              header("location:index.php");
+            }else{
+              die("Failed to add user ". mysqli_error($db) );
+            }
+
+
+
+          }
+
+
+
+
+
+        } //isset if close
+      ?>
 
       <a href="index.php" class="text-center">I already have a membership</a>
     </div>
@@ -91,5 +182,8 @@
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
 <script src="dist/js/adminlte.min.js"></script>
+<?php
+  ob_end_flush();
+?>
 </body>
 </html>
